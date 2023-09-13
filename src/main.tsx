2b2ba0db489b196dcae00e439643c8b3ac2e214f
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-09-13 12:13:09 +0545

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
 import App from "./App1.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
